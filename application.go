d43734b12c411bbc7e14091bddf52ae02a29482f
Dan Mayor <dmayor@digivance.com>
2017-10-22 16:13:24 -0400

    Updating commenting, refactoring Redirect to RedirectSecure and implementing RedirectSecureJS and ForceSecureJS methods

--- application.go
+++ application.go
@@ -28,9 +28,17 @@ type Application struct {
 	// RouteManager is our Route Manager system
 	RouteManager *RouteManager
 
+	// DomainName is used by methods that generate full links, such as redirect secure methods
+	DomainName string
+
+	// BindAddress is the ip address that this application will listen on
 	BindAddress string
-	HTTPPort    int
-	HTTPSPort   int
+
+	// HTTPPort is the port used to stream plain text http protocol
+	HTTPPort int
+
+	// HTTPSPort is the port used to stream TLS secured http protocol
+	HTTPSPort int
 }
 
 // NewApplication returns a new default MVC Application object
@@ -65,22 +73,37 @@ func (app *Application) RunSecure(certFile string, keyFile string) error {
 // socket servers)
 func (app *Application) RunForcedSecure(certFile string, keyFile string) error {
 	addr := fmt.Sprintf("%s:%d", app.BindAddress, app.HTTPPort)
-	go http.ListenAndServe(addr, http.HandlerFunc(redirect))
+	go http.ListenAndServe(addr, http.HandlerFunc(app.RedirectSecure))
 
 	addr = fmt.Sprintf("%s:%d", app.BindAddress, app.HTTPSPort)
 	return http.ListenAndServeTLS(addr, certFile, keyFile, http.HandlerFunc(app.RouteManager.HandleRequest))
 }
 
-// redirect is used internally to submit an http redirect from http to https when
-// forcing secure
-func redirect(w http.ResponseWriter, req *http.Request) {
-	target := "https://" + req.Host + req.URL.Path
+// RunForcedSecureJS is used to run a web application in forced TLS secure mode by returning
+// a simple page with javascript that redirects the browser to https://DomainName/path
+func (app *Application) RunForcedSecureJS(certFile string, keyFile string) error {
+	addr := fmt.Sprintf("%s:%d", app.BindAddress, app.HTTPPort)
+	go http.ListenAndServe(addr, http.HandlerFunc(app.RedirectSecureJS))
+
+	addr = fmt.Sprintf("%s:%d", app.BindAddress, app.HTTPSPort)
+	return http.ListenAndServeTLS(addr, certFile, keyFile, http.HandlerFunc(app.RouteManager.HandleRequest))
+}
+
+// RedirectSecure is used to submit an http redirect from http to https when forcing secure
+func (app *Application) RedirectSecure(w http.ResponseWriter, req *http.Request) {
+	target := fmt.Sprintf("https://%s%s", app.DomainName, req.URL.Path)
 
 	if len(req.URL.RawQuery) > 0 {
 		target += "?" + req.URL.RawQuery
 	}
 
 	http.Redirect(w, req, target, http.StatusTemporaryRedirect)
+}
+
+// RedirectSecureJS is used to submit a javascript redirect from http to https when forcing secure
+func (app *Application) RedirectSecureJS(w http.ResponseWriter, req *http.Request) {
+	data := fmt.Sprintf("<html><head><title>Redirecting to secure site mode</title></head><body><script type=\"text/javascript\">window.location.href='https://%s%s';</script></body>", app.DomainName, req.URL.Path)
+	w.Write([]byte(data))
 }
 
 var appPath = ""
